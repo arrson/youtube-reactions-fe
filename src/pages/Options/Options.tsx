0a5d197dab92224968c26f6739f51a382f4367e5
Stephen Arra <stephen@Stephens-MacBook-Pro.local>
2022-08-23 13:14:24 -0500

    add options example

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Options.css';
 
-interface Props {
-  title: string;
+interface ICreator {
+  value: string;
 }
 
-const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+interface CreatorProps {
+  value: string;
+  onChange: Function;
+}
+
+interface OptionsProps {
+  title: string; 
+}
+
+const Creator = ({ value, onChange } : CreatorProps) =>  
+  <div className="OptionsContainer">
+    <input 
+      value={value} 
+      onChange={(e) => {
+        onChange(e.currentTarget.value);
+      }} 
+    />
+  </div>;
+
+const Options = ({ title }: OptionsProps) => {
+  const [creators, setCreators] = useState<ICreator[]>([]);
+
+  return <div>
+    {creators.map((d, i) => 
+      <Creator 
+        value={d.value} 
+        onChange={(newValue: string) => {
+          const newArr = [...creators];
+          newArr[i] = { value: newValue };
+          setCreators(newArr);
+        }}
+      />
+    )}
+    <button onClick={() => {
+      setCreators([...creators, { value: '' }])
+    }}>Add</button>
+  </div>
 };
 
 export default Options;
